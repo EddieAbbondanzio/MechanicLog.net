0c5ae49cc010ce1c0bd0a02c00d5e8da2e29eeef
BERT\Eddie <me@eddieabbondanz.io>
2019-01-07 21:00:11 -0500

    Vue Attempt 2.

---
+++ src/components/nav-bar.vue
@@ -0,0 +1,69 @@
+<style scoped>
+.navbar-brand {
+  font-size: 24px;
+  font-weight: 600;
+}
+
+.logo {
+  height: 48px;
+}
+
+.nav-item {
+  font-size: 18px;
+}
+</style>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <!-- Icon and Name -->
+    <a class="navbar-brand" href="#">
+      <img alt="MechanicLog logo" class="logo" src="../assets/logo.png">
+      {{ brand }}
+    </a>
+
+    <!-- Toggle Button -->
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Menu Options -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active mx-2">
+          <router-link class="nav-link" to="/about">Features</router-link>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link text-alert" href="#">Login</a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="#">Sign Up</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'nav-bar',
+})
+export default class NavBar extends Vue {
+  /**
+   * The name of the product in the nav bar
+   */
+  public brand: string = 'MechanicLog';
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <nav-bar />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/hello-world.vue'; // @ is an alias to /src
+import NavBar from '@/components/nav-bar.vue';
 
 @Component({
   components: {
     HelloWorld,
-  },
+    NavBar,
+},
 })
 export default class Home extends Vue {}
 </script>
