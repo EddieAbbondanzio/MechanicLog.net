4b882ac6e6ac39d5b67572e50847e13c94cd60cf
EddieAbbondanzio <me@eddieabbondanz.io>
2019-02-26 20:05:48 -0500

    Added icon

--- src/core/cookie-storage.ts
+++ src/core/cookie-storage.ts
@@ -40,6 +40,6 @@ export class CookieStorage {
      * @param expiration How long the cookie should be kept for.
      */
     public static set(key: string, value: any, expiration: string | number | Date): void {
-        (window as any).$cookies.set(key, value, expiration, '/', Store.baseUrl);
+        (window as any).$cookies.set(key, value, expiration, '/');
     }
 }
